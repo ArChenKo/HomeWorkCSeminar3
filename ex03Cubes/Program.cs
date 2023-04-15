86d4a1fb8bbc0d0af57ccaecec4bb6f1646acfcb
Arslan <arslan.chen@mail.ru>
2023-04-16 02:21:37 +0400

    ex03 added

---
+++ ex03Cubes/Program.cs
@@ -0,0 +1,17 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int ReadNumber(string mes)
+{
+    Console.Write(mes + ": ");
+    int res = int.Parse(Console.ReadLine()!);
+    return res;
+}
+int a = ReadNumber("Введите число");
+int num = 1;
+int cube = 0;
+Console.WriteLine("N | N в кубе");
+while (num <= a)
+{
+    cube = num * num * num;
+    Console.WriteLine(num + " | " + cube);
+    num++;
+}
